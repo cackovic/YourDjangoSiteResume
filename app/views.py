1e3e788d39c79564d6411fe997d93b6505c967e2
Ajna Cackovic <cackovic@users.noreply.github.com>
2015-09-08 13:50:25 -0500

    Add meow rendering

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ Definition of views.
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
+from django.shortcuts import render_to_response
 from datetime import datetime
 
 YOUR_INFO = {
@@ -27,3 +28,72 @@ def home(request):
                 'year': datetime.now().year,
             })
     )
+
+def webcat(request, imgname=None, maxLen=100.0, clr=True, fontSize=7):
+    """
+    Based on http://github.com/hit9/img2txt.git
+    """
+
+    import sys
+
+    from PIL import Image
+    import cStringIO
+    import urllib
+
+    if imgname is None:
+        # imgname = "/Users/ajna/Code/YourDjangoSiteResume/cats-animals-kittens-background.jpg"
+        imgname = "http://loremflickr.com/320/240/kitten"
+
+    # img = Image.open(imgname)
+    img = Image.open(cStringIO.StringIO(urllib.urlopen(imgname).read()))
+    color_webcat = None
+    webcat = None
+
+
+    # resize to: the max of the img is maxLen
+
+    width, height = img.size
+    rate = maxLen / max(width, height)
+    width = int(rate * width)  # cast to int
+
+    height = int(rate * height)
+
+    img = img.resize((width, height))
+
+
+    # get pixels
+    pixel = img.load()
+
+    # grayscale
+    color = "MNHQ$OC?7>!:-;. "
+
+    webcat = ""
+
+    # for h in xrange(height):  # first go through the height,  otherwise will roate
+    #     for w in xrange(width):
+    #         rgb = pixel[w, h]
+    #         if clr:
+    #             color_webcat += "<span style=\"color:rgb" + str(rgb) + \
+    #                 ";\">▇</span>"
+    #         else:
+    #             webcat += color[int(sum(rgb) / 3.0 / 256.0 * 16)]
+    #     webcat += "\n"
+    #
+
+    if clr:
+        color_webcat = [[('\xe2\x96\x87', str(pixel[w, h])) for w in xrange(width)] for h in xrange(height)]
+        # color_webcat = [[(color[int(sum(pixel[w, h]) / 3.0 / 256.0 * 16)], str(pixel[w, h])) for w in xrange(width)] for h in xrange(height)]
+    # wrappe with html
+    else:
+        webcat = '\n'.join(
+            [''.join(map(str,[color[int(sum(pixel[w, h]) / 3.0 / 256.0 * 16)]
+                              for w in xrange(width)]))
+             for h in xrange(height)])
+
+
+
+    return render_to_response('app/webcat.html', context={"font_size": fontSize,
+                                                          "webcat": webcat,
+                                                          "color_webcat": color_webcat,
+                                                          'color': clr,
+                                                          })
